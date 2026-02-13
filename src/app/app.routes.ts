c1fb2dc1d05bdb4a11b18c0a3f25dfa7b1986995
les269 <pook20042004@gmail.com>
2026-02-13 09:49:49 +0800

    調整寬度一致

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -20,9 +20,9 @@ export const routes: Routes = [
   {
     path: 'api-config-list',
     loadComponent: () =>
-      import(
-        './features/api-config/page/api-config-list/api-config-list.component'
-      ).then(mod => mod.ApiConfigListComponent),
+      import('./features/api-config/page/api-config-list/api-config-list.component').then(
+        mod => mod.ApiConfigListComponent
+      ),
     data: { title: 'title.apiConfigList', sidenav: true },
   },
   {
@@ -52,81 +52,81 @@ export const routes: Routes = [
   {
     path: 'scrapy-pagination-list',
     loadComponent: () =>
-      import(
-        './features/scrapy/page/scrapy-pagination-list/scrapy-pagination-list.component'
-      ).then(mod => mod.ScrapyPaginationListComponent),
+      import('./features/scrapy/page/scrapy-pagination-list/scrapy-pagination-list.component').then(
+        mod => mod.ScrapyPaginationListComponent
+      ),
     data: { title: 'title.scrapyPaginationList', sidenav: true },
   },
   {
     path: 'scrapy-pagination-edit',
     loadComponent: () =>
-      import(
-        './features/scrapy/page/scrapy-pagination-edit/scrapy-pagination-edit.component'
-      ).then(mod => mod.ScrapyPaginationEditComponent),
+      import('./features/scrapy/page/scrapy-pagination-edit/scrapy-pagination-edit.component').then(
+        mod => mod.ScrapyPaginationEditComponent
+      ),
     data: { title: 'title.scrapyPaginationEdit' },
   },
   {
     path: 'scrapy-pagination-edit/:name',
     loadComponent: () =>
-      import(
-        './features/scrapy/page/scrapy-pagination-edit/scrapy-pagination-edit.component'
-      ).then(mod => mod.ScrapyPaginationEditComponent),
+      import('./features/scrapy/page/scrapy-pagination-edit/scrapy-pagination-edit.component').then(
+        mod => mod.ScrapyPaginationEditComponent
+      ),
     data: { title: 'title.scrapyPaginationEdit' },
   },
   {
     path: 'dataset-list',
     loadComponent: () =>
-      import(
-        './features/dataset/page/dataset-list/dataset-list.component'
-      ).then(mod => mod.DatasetListComponent),
+      import('./features/dataset/page/dataset-list/dataset-list.component').then(
+        mod => mod.DatasetListComponent
+      ),
     data: { title: 'title.datasetList', sidenav: true },
   },
   {
     path: 'dataset-edit',
     loadComponent: () =>
-      import(
-        './features/dataset/page/dataset-edit/dataset-edit.component'
-      ).then(mod => mod.DatasetEditComponent),
+      import('./features/dataset/page/dataset-edit/dataset-edit.component').then(
+        mod => mod.DatasetEditComponent
+      ),
     data: { title: 'title.datasetEdit' },
   },
   {
     path: 'dataset-edit/:name',
     loadComponent: () =>
-      import(
-        './features/dataset/page/dataset-edit/dataset-edit.component'
-      ).then(mod => mod.DatasetEditComponent),
+      import('./features/dataset/page/dataset-edit/dataset-edit.component').then(
+        mod => mod.DatasetEditComponent
+      ),
     data: { title: 'title.datasetEdit' },
   },
   {
     path: 'group-dataset-list',
     loadComponent: () =>
-      import(
-        './features/dataset/page/group-dataset-list/group-dataset-list.component'
-      ).then(mod => mod.GroupDatasetListComponent),
+      import('./features/dataset/page/group-dataset-list/group-dataset-list.component').then(
+        mod => mod.GroupDatasetListComponent
+      ),
     data: { title: 'title.groupDataset', sidenav: true },
   },
   {
     path: 'group-dataset-edit',
     loadComponent: () =>
-      import(
-        './features/dataset/page/group-dataset-edit/group-dataset-edit.component'
-      ).then(mod => mod.GroupDatasetEditComponent),
+      import('./features/dataset/page/group-dataset-edit/group-dataset-edit.component').then(
+        mod => mod.GroupDatasetEditComponent
+      ),
     data: { title: 'title.groupDatasetEdit' },
   },
   {
     path: 'group-dataset-edit/:name',
     loadComponent: () =>
-      import(
-        './features/dataset/page/group-dataset-edit/group-dataset-edit.component'
-      ).then(mod => mod.GroupDatasetEditComponent),
+      import('./features/dataset/page/group-dataset-edit/group-dataset-edit.component').then(
+        mod => mod.GroupDatasetEditComponent
+      ),
     data: { title: 'title.groupDatasetEdit' },
   },
   {
     path: 'imageList/:name/:version',
     loadComponent: () =>
-      import(
-        './features/theme/pages/image-list-view/image-list-view.component'
-      ).then(mod => mod.ImageListViewComponent),
+      import('./features/theme/pages/image-list-view/image-list-view.component').then(
+        mod => mod.ImageListViewComponent
+      ),
   },
   {
     path: 'table/:name/:version',
@@ -138,9 +138,9 @@ export const routes: Routes = [
   {
     path: 'replace-value-map',
     loadComponent: () =>
-      import(
-        './features/replace-value-map/page/replace-value-map/replace-value-map.component'
-      ).then(mod => mod.ReplaceValueMapComponent),
+      import('./features/replace-value-map/page/replace-value-map/replace-value-map.component').then(
+        mod => mod.ReplaceValueMapComponent
+      ),
     data: { title: 'title.replaceValueMap', sidenav: true },
   },
   {
@@ -154,18 +154,18 @@ export const routes: Routes = [
       {
         path: '',
         loadComponent: () =>
-          import(
-            './features/setting/page/setting-database/setting-database.component'
-          ).then(mod => mod.SettingDatabaseComponent),
+          import('./features/setting/page/setting-database/setting-database.component').then(
+            mod => mod.SettingDatabaseComponent
+          ),
         data: { subtitle: 'title.database' },
       },
       {
         path: 'disk',
         loadComponent: () =>
-          import(
-            './features/setting/page/disk-management/disk-management.component'
-          ).then(mod => mod.DiskManagementComponent),
-        data: { subtitle: 'title.disk' },
+          import('./features/setting/page/disk-management/disk-management.component').then(
+            mod => mod.DiskManagementComponent
+          ),
+        data: { title: 'title.setting', subtitle: 'title.disk' },
       },
     ],
   },
