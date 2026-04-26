36da4388e61a36263bebc304f817d4a8bda9b972
les269 <pook20042004@gmail.com>
2026-04-27 03:36:57 +0800

    refactor: 移除 Nx 並改回 Angular CLI 工作流

--- src/app/features/setting/components/setting-sidebar/setting-sidebar.component.ts
+++ src/app/features/setting/components/setting-sidebar/setting-sidebar.component.ts
@@ -1,9 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { SettingLinkComponent } from '../setting-link/setting-link.component';
-import { TranslationService } from '../../../../core/services/translation.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { Router } from '@angular/router';
-import { link } from 'fs-extra';
 
 @Component({
   selector: 'app-setting-sidebar',
